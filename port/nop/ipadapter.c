335b892eeaf654dedc1e82089d4c9e7c0f36cd0b
Philippe Coval <philippe.coval@osg.samsung.com>
2018-03-23 17:35:58 +0100

    WIP: add: port/nop/ipadapter.c (sandbox/rzr/devel/master)
    Change-Id: I3c274b26e4671eecb711cf88dfc811b0918b81af
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

---
+++ port/nop/ipadapter.c
@@ -0,0 +1,274 @@
+/*
+// Copyright (c) 2016 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#define __USE_GNU
+#include "oc_buffer.h"
+#include "oc_core_res.h"
+#include "oc_endpoint.h"
+#include "port/oc_assert.h"
+#include "port/oc_connectivity.h"
+#include <assert.h>
+#include <errno.h>
+
+/*
+  This include creates issues with some toolchains and seems not
+  necessary for compilation.
+#include <linux/ipv6.h>
+*/
+#include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+/* Some outdated toolchains do not define IFA_FLAGS.
+   Note: Requires Linux kernel 3.14 or later. */
+#ifndef IFA_FLAGS
+#define IFA_FLAGS (IFA_MULTICAST+1)
+#endif
+
+#define OCF_PORT_UNSECURED (5683)
+static const uint8_t ALL_OCF_NODES_LL[] = {
+  0xff, 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x01, 0x58
+};
+static const uint8_t ALL_OCF_NODES_RL[] = {
+  0xff, 0x03, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x01, 0x58
+};
+static const uint8_t ALL_OCF_NODES_SL[] = {
+  0xff, 0x05, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x01, 0x58
+};
+#define ALL_COAP_NODES_V4 0xe00001bb
+
+typedef struct ip_context_t
+{
+  struct ip_context_t *next;
+  int mcast_sock;
+  int server_sock;
+  uint16_t port;
+#ifdef OC_SECURITY
+  struct sockaddr_storage secure;
+  int secure_sock;
+  uint16_t dtls_port;
+#endif /* OC_SECURITY */
+#ifdef OC_IPV4
+  struct sockaddr_storage mcast4;
+  struct sockaddr_storage server4;
+  int mcast4_sock;
+  int server4_sock;
+  uint16_t port4;
+#ifdef OC_SECURITY
+  struct sockaddr_storage secure4;
+  int secure4_sock;
+  uint16_t dtls4_port;
+#endif /* OC_SECURITY */
+#endif /* OC_IPV4 */
+  int terminate;
+  int device;
+} ip_context_t;
+
+#ifdef OC_DYNAMIC_ALLOCATION
+OC_LIST(ip_contexts);
+#else /* OC_DYNAMIC_ALLOCATION */
+static ip_context_t devices[OC_MAX_NUM_DEVICES];
+#endif /* !OC_DYNAMIC_ALLOCATION */
+
+void
+oc_network_event_handler_mutex_init(void)
+{
+    OC_ERR("initializing network event handler mutex\n");
+}
+
+void
+oc_network_event_handler_mutex_lock(void)
+{
+}
+
+void
+oc_network_event_handler_mutex_unlock(void)
+{
+}
+
+static ip_context_t *
+get_ip_context_for_device(int device)
+{
+#ifdef OC_DYNAMIC_ALLOCATION
+  ip_context_t *dev = oc_list_head(ip_contexts);
+  while (dev != NULL && dev->device != device) {
+    dev = dev->next;
+  }
+  if (!dev) {
+    return NULL;
+  }
+#else  /* OC_DYNAMIC_ALLOCATION */
+  ip_context_t *dev = &devices[device];
+#endif /* !OC_DYNAMIC_ALLOCATION */
+  return dev;
+}
+
+static void *
+network_event_thread(void *data)
+{
+}
+
+static void
+get_interface_addresses(unsigned char family, uint16_t port, bool secure)
+{
+}
+
+oc_endpoint_t *
+oc_connectivity_get_endpoints(int device)
+{
+}
+
+void
+oc_send_buffer(oc_message_t *message)
+{
+}
+
+#ifdef OC_CLIENT
+void
+oc_send_discovery_request(oc_message_t *message)
+{
+}
+#endif /* OC_CLIENT */
+
+#ifdef OC_IPV4
+static int
+connectivity_ipv4_init(ip_context_t *dev)
+{
+  OC_DBG("Initializing IPv4 connectivity for device %d\n", dev->device);
+  memset(&dev->mcast4, 0, sizeof(struct sockaddr_storage));
+  memset(&dev->server4, 0, sizeof(struct sockaddr_storage));
+
+  struct sockaddr_in *m = (struct sockaddr_in *)&dev->mcast4;
+  m->sin_family = AF_INET;
+  m->sin_port = htons(OCF_PORT_UNSECURED);
+  m->sin_addr.s_addr = INADDR_ANY;
+
+  struct sockaddr_in *l = (struct sockaddr_in *)&dev->server4;
+  l->sin_family = AF_INET;
+  l->sin_addr.s_addr = INADDR_ANY;
+  l->sin_port = 0;
+
+#ifdef OC_SECURITY
+  memset(&dev->secure4, 0, sizeof(struct sockaddr_storage));
+  struct sockaddr_in *sm = (struct sockaddr_in *)&dev->secure4;
+  sm->sin_family = AF_INET;
+  sm->sin_port = 0;
+  sm->sin_addr.s_addr = INADDR_ANY;
+
+  dev->secure4_sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if (dev->secure4_sock < 0) {
+    OC_ERR("creating secure IPv4 socket\n");
+    return -1;
+  }
+#endif /* OC_SECURITY */
+
+  dev->server4_sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  dev->mcast4_sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+  if (dev->server4_sock < 0 || dev->mcast4_sock < 0) {
+    OC_ERR("creating IPv4 server sockets\n");
+    return -1;
+  }
+
+  if (bind(dev->server4_sock, (struct sockaddr *)&dev->server4,
+           sizeof(dev->server4)) == -1) {
+    OC_ERR("binding server4 socket %d\n", errno);
+    return -1;
+  }
+
+  socklen_t socklen = sizeof(dev->server4);
+  if (getsockname(dev->server4_sock, (struct sockaddr *)&dev->server4,
+                  &socklen) == -1) {
+    OC_ERR("obtaining server4 socket information %d\n", errno);
+    return -1;
+  }
+
+  dev->port4 = ntohs(l->sin_port);
+
+  struct ip_mreq mreq;
+  memset(&mreq, 0, sizeof(mreq));
+  mreq.imr_multiaddr.s_addr = htonl(ALL_COAP_NODES_V4);
+  if (setsockopt(dev->mcast4_sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq,
+                 sizeof(mreq)) == -1) {
+    OC_ERR("joining IPv4 multicast group %d\n", errno);
+    return -1;
+  }
+
+  int reuse = 1;
+  if (setsockopt(dev->mcast4_sock, SOL_SOCKET, SO_REUSEADDR, &reuse,
+                 sizeof(reuse)) == -1) {
+    OC_ERR("setting reuseaddr IPv4 option %d\n", errno);
+    return -1;
+  }
+  if (bind(dev->mcast4_sock, (struct sockaddr *)&dev->mcast4,
+           sizeof(dev->mcast4)) == -1) {
+    OC_ERR("binding mcast IPv4 socket %d\n", errno);
+    return -1;
+  }
+
+#ifdef OC_SECURITY
+  if (setsockopt(dev->secure4_sock, SOL_SOCKET, SO_REUSEADDR, &reuse,
+                 sizeof(reuse)) == -1) {
+    OC_ERR("setting reuseaddr IPv4 option %d\n", errno);
+    return -1;
+  }
+
+  if (bind(dev->secure4_sock, (struct sockaddr *)&dev->secure4,
+           sizeof(dev->secure4)) == -1) {
+    OC_ERR("binding IPv4 secure socket %d\n", errno);
+    return -1;
+  }
+
+  socklen = sizeof(dev->secure4);
+  if (getsockname(dev->secure4_sock, (struct sockaddr *)&dev->secure4,
+                  &socklen) == -1) {
+    OC_ERR("obtaining DTLS4 socket information %d\n", errno);
+    return -1;
+  }
+
+  dev->dtls4_port = ntohs(sm->sin_port);
+#endif /* OC_SECURITY */
+
+  OC_DBG("Successfully initialized IPv4 connectivity for device %d\n",
+         dev->device);
+
+  return 0;
+}
+#endif
+
+static int
+add_mcast_sock_to_ipv6_multicast_group(int sock, const uint8_t *addr)
+{
+
+}
+
+int
+oc_connectivity_init(int device)
+{
+}
+
+void
+oc_connectivity_shutdown(int device)
+{
+  ip_context_t *dev = get_ip_context_for_device(device);
+  dev->terminate = 1;
+
+  close(dev->server_sock);
+  close(dev->mcast_sock);
+
+  OC_DBG("oc_connectivity_shutdown for device %d\n", device);
+}
